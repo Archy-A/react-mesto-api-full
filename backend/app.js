39c882d7525148b45210fe8f182f2d5c9fbc0048
Artur Avtukhov <archy6754267@gmail.com>
2023-02-03 20:46:17 +0100

    merging_CORS23

--- backend/app.js
+++ backend/app.js
@@ -18,14 +18,17 @@ const allowedCors = [
     'http://testdeploy.nomoredomainsclub.ru',
     'localhost:3000'
   ];
+
 app.use(function(req, res, next) {
+  const { method } = req
   const { origin } = req.headers;
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const requestHeaders = req.headers['access-control-request-headers'];
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  if (metho === 'OPTIONS') {
+  if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
