8c3e4a69f399ebc40f9aa440e7d44d21e4d506de
Artur Avtukhov <archy6754267@gmail.com>
2023-02-03 09:22:06 +0100

    Logger10

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -3,18 +3,18 @@ const expressWinston = require('express-winston');
 
 const requestLogger = expressWinston.logger({
   transports: [
-    // new winston.transports.File({ filename: '../texterrors/request.log' }),
+    new winston.transports.File({ filename: './texterrors/request.log' }),
     // new winston.transports.File({ filename: '/home/art/react-mesto-api-full/backend/texterrors/request.log' }),
-    new winston.transports.Console()
+    //new winston.transports.Console()
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    // new winston.transports.File({ filename: '../texterrors/error.log' }),
+  new winston.transports.File({ filename: './texterrors/error.log' }),
     // new winston.transports.File({ filename: '/home/art/react-mesto-api-full/backend/texterrors/error.log' }),
-    new winston.transports.Console()
+    //new winston.transports.Console()
   ],
   format: winston.format.json(),
 });
