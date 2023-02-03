ce339b1a648d7b672b65fac299b3b0c09b1786c8
Artur Avtukhov <archy6754267@gmail.com>
2023-02-03 20:35:28 +0100

    merging_CORS21

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,12 @@ const allowedCors = [
 
 app.use(function(req, res, next) {
   const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
   }
   next();
 });
