79ee1aa57e2722beb2df49cbf222676c4fb83929
Artur Avtukhov <archy6754267@gmail.com>
2023-02-03 07:41:33 +0100

    CORS_02

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,14 @@ const allowedCors = [
   'localhost:3000'
 ];
 app.use(function(req, res, next) {
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
+  }
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
